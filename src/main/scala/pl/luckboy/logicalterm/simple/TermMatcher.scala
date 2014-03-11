57e21fb3c2a7565a055af5b163c82592381abe71
Lukasz Szpakowski <luckboy@vp.pl>
2014-03-11 18:39:58 +0100

    Optimization.

--- src/main/scala/pl/luckboy/logicalterm/simple/TermMatcher.scala
+++ src/main/scala/pl/luckboy/logicalterm/simple/TermMatcher.scala
@@ -5,7 +5,7 @@ import pl.luckboy.logicalterm._
 
 class TermMatcher extends Matcher[Term]
 {
-  override def matchingTermFromTerm(term: Term) = some(term)
+  override def matchingTermFromTerm(term: Term) = some(term.normalizedTerm)
   
   private def matchesForOneTerm(term: Term, terms: Set[Term], matching: Matching.Value) =
     terms.foldLeft(false) {
@@ -17,30 +17,28 @@ class TermMatcher extends Matcher[Term]
       (b, term2) => if(b) matchesTerms(term, term2, matching) else false
     }
   
-  private def matchesSupertermWithTerm(term1: Term, term2: Term): Boolean = {
-    val (normalizedTerm1, normalizedTerm2) = (term1.normalizedTerm, term2.normalizedTerm)
-    (normalizedTerm1, normalizedTerm2) match {
+  private def matchesSupertermWithTerm(term1: Term, term2: Term): Boolean =
+    (term1, term2) match {
       case (Disjunction(terms1), Conjunction(terms2)) =>
-        normalizedTerm1.distributedTerm.map { (_, normalizedTerm2) }.orElse {
-          normalizedTerm2.distributedTerm.map { (normalizedTerm1, _) }
+        term1.distributedTerm.map { (_, term2) }.orElse {
+          term2.distributedTerm.map { (term1, _) }
         }.map {
           case (distributedTerm1, distributedTerm2) => matchesSupertermWithTerm(distributedTerm1, distributedTerm2)
         }.getOrElse {
-          matchesForOneTerm(normalizedTerm1, terms2, Matching.SupertermWithTerm) ||
-          matchesForOneTerm(normalizedTerm2, terms1, Matching.TermWithSuperterm)
+          matchesForOneTerm(term1, terms2, Matching.SupertermWithTerm) ||
+          matchesForOneTerm(term2, terms1, Matching.TermWithSuperterm)
         }
       case (_, Disjunction(terms2)) =>
-        matchesForAllTerms(normalizedTerm1, terms2, Matching.SupertermWithTerm)
+        matchesForAllTerms(term1, terms2, Matching.SupertermWithTerm)
       case (Conjunction(terms1), _) =>
-        matchesForAllTerms(normalizedTerm2, terms1, Matching.TermWithSuperterm)
+        matchesForAllTerms(term2, terms1, Matching.TermWithSuperterm)
       case (_, Conjunction(terms2)) =>
-        matchesForOneTerm(normalizedTerm1, terms2, Matching.SupertermWithTerm)
+        matchesForOneTerm(term1, terms2, Matching.SupertermWithTerm)
       case (Disjunction(terms1), _) =>
-        matchesForOneTerm(normalizedTerm2, terms1, Matching.TermWithSuperterm)
+        matchesForOneTerm(term2, terms1, Matching.TermWithSuperterm)
       case (varApp1: VarApp, varApp2: VarApp) =>
         matchesVarApps(varApp1, varApp2)
     }
-  }
   
   private def matchesVarApps(varApp1: VarApp, varApp2: VarApp) =
     (varApp1, varApp2) match {
